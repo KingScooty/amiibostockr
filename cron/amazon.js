52880f5e497edc0d988b367271ee1e9fd1c6928d
Scotty Vernon <scott.vernon@gmail.com>
2015-04-15 11:21:04 +0100

    Comments

--- cron/amazon.js
+++ cron/amazon.js
@@ -7,13 +7,23 @@ var settings = require('./settings/settings');
 
 var self = module.exports = {
 
+  /**
+   * create_client() creates a new amazonAPI client.
+   *
+   * @params {Object} credentials
+   * @returns {Object} amazon API client
+   */
+
   create_client: function create_client(credentials) {
     return amazonAPI.createClient(credentials);
   },
 
-  /*
-   * query(object, array, string);
-   * query(client_UK, [1,2,3], 'domain.com');
+  /**
+   * query() calls the amazon API using the given params
+   * and returns a response. Can query a max of 10 products at a time.
+   *
+   * @params {Object, Array, String} client, product_array, domain
+   * @return {response} response
    */
 
   query: function query(client, product_array, domain) {
@@ -37,33 +47,46 @@ var self = module.exports = {
     });
   },
 
-  /*
-   * batch_query('UK')
-   * Generate args from settings file and return response
+  /**
+   * batch_query() returns a callback once all queries
+   * have returned, and have been batched together into
+   * one single, flattened object.
+   *
+   * @param {string} locale
+   * @return {payload} payload
    */
 
   batch_query: function batch_query(locale) {
     /*
-     * Batch calls a query and waits for all promises to complete.
-     * Call following function number of times and wait for
-     * all promises to complete.
+     * Generate args based on locale.
+     * Grab constants from relevant locale settings file.
      */
-
     var args = {
       client: self.create_client(settings[locale].CREDS),
       product_chunks: settings[locale].ASIN_CHUNKS,
       domain: settings[locale].DOMAIN
     };
 
+    /*
+     * Wait for all queries to return before returning callback
+     */
     return Promise.all(args.product_chunks.map(function callback() {
+      /*
+       * Return response for one query
+       */
       return self.query(args);
     }))
 
-    // Flatten payload chunks into one array
+    /*
+     * Flatten payload chunks into one array
+     */
     .then(function callback(payload) {
       return _.flatten(payload);
     })
 
+    /*
+     * Catch errors.
+     */
     .catch(function callback(e) {
       console.log('Exception ' + e);
     });
