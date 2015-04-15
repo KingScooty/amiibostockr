fa6bb3f02939cfab8fdded5a77699ecf5b0a6c2d
Scotty Vernon <scott.vernon@gmail.com>
2015-04-15 15:48:01 +0100

    Add base settings for CI and cleanup old files.

--- test/stock.test.js
+++ test/stock.test.js
@@ -2,13 +2,9 @@
 'use strict';
 var assert = require('chai').assert;
 
-var _ = require('lodash');
-var O = require('observed');
-var sinon = require('sinon');
-
-// var STOCK = require('../cron/stock').STOCK;
-var stock = require('../cron/stock');
-var STOCK = stock.STOCK;
+// var _ = require('lodash');
+// var O = require('observed');
+// var sinon = require('sinon');
 
 
 describe('Stock Module', function() {
